76f350509b2be0c163166c2860d94c0b3e08c6e3
Dongmin Kim <macaw2469@gmail.com>
2023-02-06 14:34:53 +0900

    Style : Tailwind를 이용한 NewProducts 스타일링
    기본적인 NewProducts UI를 위해 TailWind CSS라이브러리를 이용하여 스타일링 구현
    
        - NewProducts section태그 마진,패딩 추가
        - h1태그 텍스트크기 두께 라인 스타일 추가
        - form태그 width및 flex 스타일 추가
        - img file input태그 required속성 추가
        - Input UI 컴포넌트에서 div, label, input스타일링 추가
        - Button UI 컴포넌트의 스타일링을 추가하기 위해 props에 className추가
        - Button 컴포넌트 className props으로 스타일 추가

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, className }) {
     return (
         <button
-            className=" bg-brand text-white px-2 py-1 rounded-lg
-            hover:scale-95 hover:opacity-90"
+            className={
+                ' bg-brand text-white px-2 py-1 rounded-lg hover:scale-95 hover:opacity-90 ' +
+                className
+            }
             onClick={onClick}
         >
             {text}
--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 export default function Input({ id, text, placeholder }) {
     return (
-        <div>
-            <label htmlFor={id}>{text}</label>
-            <input type="text" id={id} placeholder={placeholder} required />
+        <div className="flex gap-2 my-2 w-full">
+            <label htmlFor={id} className={'text-xl'}>
+                {text}
+            </label>
+            <input
+                className="w-1/2 pl-2"
+                type="text"
+                id={id}
+                placeholder={placeholder}
+                required
+            />
         </div>
     );
 }
