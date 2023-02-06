c1fe1e6dfdd8ce273c9671e47929ec408397cd3e
Dongmin Kim <macaw2469@gmail.com>
2023-02-06 14:34:53 +0900

    Style : Tailwind를 이용한 NewProducts 스타일링
    기본적인 NewProducts UI를 위해 TailWind CSS라이브러리를 이용하여 스타일링 구현
    
        - NewProducts section태그 마진,패딩 추가
        - h1태그 텍스트크기 두께 라인 스타일 추가
        - form태그 width및 flex 스타일 추가
        - img file input태그 required속성 추가
        - Input UI 컴포넌트에서 div, label, input스타일링 추가
        - Button UI 컴포넌트의 스타일링을 추가하기 위해 props에 className추가
        - Button 컴포넌트 className props으로 스타일 추가

--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -4,10 +4,19 @@ import Input from '../components/ui/Input';
 
 export default function NewProducts() {
     return (
-        <section>
-            <h1>제품 추가</h1>
-            <form>
-                <input type="file" />
+        <section className="m-2 p-2">
+            <h1
+                className="text-4xl text-center font-bold border-b-2 border-brand border-opacity-50 my-4 py-4
+            "
+            >
+                제품 추가
+            </h1>
+            <form className="w-full flex flex-col">
+                <input
+                    type="file"
+                    className="my-4 border-2 p-2 py-4 rounded-md"
+                    required
+                />
                 <Input
                     id={'테스트'}
                     text={'테스트 : '}
@@ -33,7 +42,10 @@ export default function NewProducts() {
                     text={'제품 옵션 : '}
                     placeholder={'옵션은 콤마(,)로 구분하여 입력하세요.'}
                 />
-                <Button text={'제품 추가하기'} />
+                <Button
+                    text={'제품 추가하기'}
+                    className={'my-6 py-2 font-semibold text-xl'}
+                />
             </form>
         </section>
     );
