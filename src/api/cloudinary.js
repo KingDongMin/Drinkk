6fe9aab476d2608ee76bc8bfe784ae4a61438dff
Dongmin Kim <macaw2469@gmail.com>
2023-02-08 15:52:14 +0900

    Refactor: Cloudinary.js에서 test code 삭제

--- src/api/cloudinary.js
+++ src/api/cloudinary.js
@@ -1,42 +1,7 @@
-import {Cloudinary} from '@cloudinary/url-gen';
-import axios from "axios"
 
 const CLOUD_NAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
 const UPLOAD_PRESET = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
-
-const cld = new Cloudinary({
-    cloud:{
-        cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME
-    }
-})
-
-const myImage = cld.image("samples/animals")
-
-export function test(){
-    console.log(myImage.toURL());
-    return myImage.toURL();
-}
-
-// cloudinary에 이미지 저장하기
-export async function addTest(formData){
-    axios.post(`http://res.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/project/drinkk/image/upload`,{
-        file : formData
-    })//
-    .then(result=>{console.log(result)})//
-    .catch(error=>{console.log(error)});
-}
-
 const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
-
-export async function submitTest(formData){
-    fetch(url, {
-        method : "POST",
-        body: formData
-    }).then(res=>res.json()
-    ).then(data=>{
-        console.log(data.url);
-    })
-}
 
 export async function addCloudinaryImg(imageURL){
     const formData = new FormData();
@@ -50,6 +15,5 @@ export async function addCloudinaryImg(imageURL){
     .then(res=>res.json())
     .then(data=>data.url)
     .catch(error=>{console.log(error)});
-
 }
 
