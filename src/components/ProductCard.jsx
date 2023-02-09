ada69b4ffd1b416e018809cb94bd56cd45ff9463
Dongmin Kim <macaw2469@gmail.com>
2023-02-09 17:38:33 +0900

    Feat: ProductCard 클릭 시 ProductDetail페이지 이동
    ProductCard를 클릭 하면 해당 제품을 자세히 볼 수 있는 ProductDetail로 이동
    
        - 제품 상세 페이지 ProductDetail 생성
        - ProductDetail로 이동하기 위해 path가 product/:id인 productDetail router Children 생성
        - ProductCard에서 id props을 이용하여 ProductDetail로 이동하기 위해 router Link 생성
        - Link의 to에 path를 통해 ProductDetail페이지로 이동 구현

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function ProductCard({ product: { imgURL, price, title } }) {
+export default function ProductCard({ product: { imgURL, price, title, id } }) {
     return (
-        <div className=" drop-shadow-md border-2 rounded-lg overflow-hidden">
-            <img
-                className="w-full h-5/6 object-cover"
-                src={imgURL}
-                alt={title}
-            />
-            <div className="flex flex-col p-2">
-                <h3 className="font-bold">{title}</h3>
-                <p className="text-sm opacity-70">￦ {price}</p>
+        <Link to={`product/${id}`}>
+            <div className=" drop-shadow-md border-2 rounded-lg overflow-hidden">
+                <img
+                    className="w-full h-5/6 object-cover"
+                    src={imgURL}
+                    alt={title}
+                />
+                <div className="flex flex-col p-2">
+                    <h3 className="font-bold">{title}</h3>
+                    <p className="text-sm opacity-70">￦ {price}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
