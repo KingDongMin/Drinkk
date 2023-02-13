4084228e7c4213e8d2a245821fc5582409bfe216
Dongmin Kim <macaw2469@gmail.com>
2023-02-13 20:21:27 +0900

    Feat : Cart페이지의 총 가격 구현
    Cart페이지에서 각 제품들의 가격과 수량에 따라 가격을 계산하여 보여지도록 구현
        - 각 제품들의 가격과 수량을 통해 총 제품 가격을 계산하기 위해서 Cart에 productPriceCalc함수를 생성
        - ProductPrice와 DERVARY_FEE, totalPrice변수를 생성하여 ProductCard에 전달
        - ProductPrice에 productPriceCalc함수를 초기화
        - Cart의 가격 섹션과 ProductCard의 기본적인 스타일링 개선

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function PriceCard({ text, price }) {
     return (
-        <div className="flex flex-col border-2 items-center py-2 px-1">
-            <h1 className="text-2xl">{text}</h1>
+        <div className="flex flex-col items-center py-4 px-2 rounded-md">
+            <h1 className="text-2xl my-2 pb-2 border-b-2 font-bold">{text}</h1>
             <p className="text-brand">￦ {price}</p>
         </div>
     );
